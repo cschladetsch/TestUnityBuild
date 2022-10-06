e5be29492d4d83e3e66fa5717cfb9a39143e2982
Christian Schladetsch <cschladetsch@powercor.com.au>
2022-10-06 17:23:37 +1100

    Added SmokeTest and Builder classes.

---
+++ Assets/App/Varsity/SmokeTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SmokeTest : MonoBehaviour
+{
+    public int NumSteps;
+
+    void Awake() {
+        Debug.Log(string.Format("{0}", "SmokeTest"));
+    }
+
+    void Start() {
+        Debug.Log(string.Format("{0}", "SmokeTest.Start"));
+    }
+
+    void OnDestroy() {
+        Debug.Log(string.Format("{0}", "SmokeTest.Destroyed"));
+    }
+
+    void Update() {
+        if (Time.frameCount > NumSteps) {
+            UnityEngine.Application.Quit(0);
+            DestroyImmediate(gameObject);
+	    }
+
+        Debug.Log(string.Format("{0}", Time.frameCount));
+    }
+}
+
---
+++ Assets/Editor/Varsity/Builder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Varsity
+{
+    public static class Builder { 
+	    public static int Build() {
+			Debug.Log("...building from Unity3d");
+			return 0;
+	    }
+    }
+}
+
